9c3d9657eed838a0eb374d90fe288479dacc7623
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-26 15:54:24 -0700

    plant seeds

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -35,7 +35,7 @@ Product.init(
       }
     },
     category_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       references: {
         model: "category", 
         key: "id"
