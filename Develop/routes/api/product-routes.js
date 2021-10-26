c35c7d51760c5e231a10541f2c0d24c208bcc62e
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-26 15:43:37 -0700

    router delete for products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -118,6 +118,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(response => {
+    res.status(200).json(response)
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  }) 
 });
 
 module.exports = router;
