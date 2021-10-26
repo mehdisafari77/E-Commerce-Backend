5bfb13067d466372d24f1ab720dc94f4ac34d7e8
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-26 15:41:03 -0700

    first router get for products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,8 +6,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
-});
+  Product.findAll({
+    include: [Category, 
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  }).then(response => {
+    res.json(response)
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  }) });
 
 // get one product
 router.get('/:id', (req, res) => {
